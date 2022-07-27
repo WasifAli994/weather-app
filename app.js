4d39aa24b3037f625bf370cde34ac252774e6217
WasifAli994 <mwasifali994@gmail.com>
2022-07-27 19:01:51 +0500

    Search by city and api response functionality implemented

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import request from 'request';
-import express from 'express';
 
-//const app = express();
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
 
-const url = "http://api.weatherapi.com/v1/current.json?key=a962b3979eb24518a26194258222603&q=Lahore&aqi=no"
+//const url = 'http://api.weatherapi.com/v1/current.json?key=a962b3979eb24518a26194258222603&q='+inputValue.value+'&aqi=no'
 
 //No need to parse the data to JSON because the data from the API is already in the json format
-request({url: url}, (error, response) =>{
+
+/*request({url: url}, (error, response) =>{
     const data = JSON.parse(response.body);
     if(error){
         console.log("There was an error connecting to the wheather app!");
     }
     console.log(data.current);
 })
+*/
+button.addEventListener('click', function(){
 
-//app.listen(3000, () => console.log("The server started on port 3000"));
+    fetch('http://api.weatherapi.com/v1/current.json?key=a962b3979eb24518a26194258222603&q='+inputValue.value+'&aqi=no')
+.then(resp => resp.json())
+.then(data => console.log(data))
+.catch(err => alert("wrong city name!"))
+})
